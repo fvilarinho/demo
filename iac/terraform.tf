25a11773eb4c1e4ce9d7b62a19ca713997b799bb
fvilarinho <fvilarinho@gmail.com>
2022-04-25 07:42:16 -0300

    Adicionando provisionamento dos servidores.

--- iac/terraform.tf
+++ iac/terraform.tf
@@ -48,13 +48,13 @@ resource "digitalocean_droplet" "cluster-manager" {
       "apt -y install curl wget htop unzip dnsutils",
       "export K3S_TOKEN=${var.k3s_token}",
       "curl -sfL https://get.k3s.io | sh -",
-      "kubectl apply -n portainer -f https://raw.githubusercontent.com/portainer/k8s/master/deploy/manifests/portainer/portainer-lb.yaml",
-      "export DD_AGENT_MAJOR_VERSION=7",
-      "export DD_API_KEY=${var.datadog_agent_key}",
-      "export DD_SITE=datadoghq.com",
-      "curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh -o install_script.sh",
-      "chmod +x ./install_script.sh",
-      "./install_script.sh"
+      "kubectl apply -n portainer -f https://raw.githubusercontent.com/portainer/k8s/master/deploy/manifests/portainer/portainer-lb.yaml" #,
+#      "export DD_AGENT_MAJOR_VERSION=7",
+#      "export DD_API_KEY=${var.datadog_agent_key}",
+#      "export DD_SITE=datadoghq.com",
+#      "curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh -o install_script.sh",
+#      "chmod +x ./install_script.sh",
+#      "./install_script.sh"
     ]
 
     connection {
@@ -83,13 +83,13 @@ resource "linode_instance" "cluster-worker" {
       "apt -y install curl wget htop unzip dnsutils",
       "export K3S_URL=https://${digitalocean_droplet.cluster-manager.ipv4_address}:6443",
       "export K3S_TOKEN=${var.k3s_token}",
-      "curl -sfL https://get.k3s.io | sh -",
-      "export DD_AGENT_MAJOR_VERSION=7",
-      "export DD_API_KEY=${var.datadog_agent_key}",
-      "export DD_SITE=datadoghq.com",
-      "curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh -o install_script.sh",
-      "chmod +x ./install_script.sh",
-      "./install_script.sh"
+      "curl -sfL https://get.k3s.io | sh -" #,
+#      "export DD_AGENT_MAJOR_VERSION=7",
+#      "export DD_API_KEY=${var.datadog_agent_key}",
+#      "export DD_SITE=datadoghq.com",
+#      "curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh -o install_script.sh",
+#      "chmod +x ./install_script.sh",
+#      "./install_script.sh"
     ]
 
     connection {
