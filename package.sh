1547e07090aa57d3d64395fc533a160ad55196b9
Felipe Vilarinho <fvilarinho@gmail.com>
2022-05-14 09:17:51 -0300

    Bugs.

--- package.sh
+++ package.sh
@@ -10,6 +10,6 @@ docker-compose -f ./iac/docker-compose.yml build
 # Save images locally.
 rm -f /tmp/demo-*.tar
 
-docker save $REPOSITORY_ID/demo-database:latest -o /tmp/demo-database.tar
-docker save $REPOSITORY_ID/demo-backend:latest -o /tmp/demo-backend.tar
-docker save $REPOSITORY_ID/demo-frontend:latest -o /tmp/demo-frontend.tar
+docker save $REPOSITORY_URL/$REPOSITORY_ID/demo-database:latest -o /tmp/demo-database.tar
+docker save $REPOSITORY_URL/$REPOSITORY_ID/demo-backend:latest -o /tmp/demo-backend.tar
+docker save $REPOSITORY_URL/$REPOSITORY_ID/demo-frontend:latest -o /tmp/demo-frontend.tar
