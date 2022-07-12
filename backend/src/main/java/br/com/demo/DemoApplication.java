386d32454f98b1e880bd8355973d7f8480dea70d
Felipe Vilarinho <fvilarinho@users.noreply.github.com>
2022-07-11 22:18:21 -0300

    Update DemoApplication.java

--- backend/src/main/java/br/com/demo/DemoApplication.java
+++ backend/src/main/java/br/com/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Class that defines the startup of the application.
  *
  * @author fvilarinho
- * @version 1.0.0
+ * @version 1.0.1
  */
 @SpringBootApplication
 public class DemoApplication {
